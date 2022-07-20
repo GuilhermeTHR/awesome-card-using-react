6c516c29d70eae8c277ae88ac0cb44bac06188c3
Guilherme Matos <guilherme.clans@gmail.com>
2022-07-19 23:40:55 -0300

    My first commit using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        layout
+        onClick={() => setIsOpen(!isOpen)}
+        className="card"
+        style={{
+          borderRadius: "1rem",
+          boxShadow: "0px 10px 30px rgba(0,0,0, 0.5)",
+        }}
+      >
+        <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="expand"
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+              quaerat a nemo veniam cupiditate illo expedita odio laboriosam
+              blanditiis tempore.{" "}
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat,
+              voluptatibus. Eum voluptate sint nobis esse eos voluptas cum illo
+              odit!
+            </p>
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
